20da8a8fc56c90f227e1356f53ff79accb1935d5
liangjiang <xuan03121994@163.com>
2018-03-19 17:51:51 +0800

    function expand

--- es6-ryf/dist/06.function-expand.js
+++ es6-ryf/dist/06.function-expand.js
@@ -103,3 +103,192 @@ function throwIfMissing() {
 // foo(1)
 
 /* 2.rest 参数 */
+function add() {
+  for (var _len = arguments.length, values = Array(_len), _key = 0; _key < _len; _key++) {
+    values[_key] = arguments[_key];
+  }
+
+  console.log(values);
+  var sum = 0;
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = values[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var val = _step.value;
+
+      sum += val;
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  return sum;
+}
+// console.log(add(1,8,10));
+
+// rest 参数之后不能再有其他参数（即只能是最后一个参数），否则会报错。
+// 函数的length属性，不包括 rest 参数。
+
+/* 3.严格模式 */
+// ES5
+function doSomething(a, b) {
+  "use strice";
+}
+// ES6
+// 规定只要函数参数使用了默认值、解构赋值、或者扩展运算符，
+// 那么函数内部就不能显式设定为严格模式，否则会报错。
+
+/* 4.name 属性 */
+// 函数的name属性，返回该函数的函数名。
+function foo() {}
+foo.name; // "foo"
+
+/* 5.箭头函数 */
+// ES6 允许使用“箭头”（=>）定义函数。
+var f = function f(v) {
+  return v;
+};
+var f = function f() {
+  return 5;
+};
+var sum = function sum(n1, n2, n3) {
+  return n1 + n2 + n3;
+};
+var sum = function sum(a, b) {
+  return (a + b) * a;
+};
+var fn = function fn() {
+  return void doesNotReturn();
+};
+
+// 箭头函数可以与变量解构结合使用
+var full = function full(_ref3) {
+  var first = _ref3.first,
+      last = _ref3.last;
+  return first + '&' + last;
+};
+
+// 使表达跟简洁
+var isEven = function isEven(n) {
+  return n % 2 == 0;
+};
+var square = function square(n) {
+  return n * n;
+};
+
+// 用于简化回调函数
+// 正常函数写法
+[1, 2, 3].map(function (x) {
+  return x * x;
+});
+// 箭头函数写法
+[1, 2, 3].map(function (x) {
+  return x * x;
+});
+
+// 简化排序函数
+// 正常函数写法
+var result = [1, 2, 4, 8].sort(function (a, b) {
+  return a - b;
+});
+// 箭头函数写法
+var result = [1, 2, 4, 8].sort(function (a, b) {
+  return a - b;
+});
+
+// rest 参数与箭头函数结合的例子
+var numbers = function numbers() {
+  for (var _len2 = arguments.length, n = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+    n[_key2] = arguments[_key2];
+  }
+
+  return n;
+};
+// console.log(numbers(1,2,3,4,5,6));
+
+var headAndTail = function headAndTail(head) {
+  for (var _len3 = arguments.length, tail = Array(_len3 > 1 ? _len3 - 1 : 0), _key3 = 1; _key3 < _len3; _key3++) {
+    tail[_key3 - 1] = arguments[_key3];
+  }
+
+  return [head, tail];
+};
+// console.log(headAndTail(1, 2, 3, 4, 5));
+
+// 箭头函数的使用注意点
+/*
+（1）函数体内的this对象，就是定义时所在的对象，而不是使用时所在的对象。
+（2）不可以当作构造函数，也就是说，不可以使用new命令，否则会抛出一个错误。
+（3）不可以使用arguments对象，该对象在函数体内不存在。如果要用，可以用 rest 参数代替。
+（4）不可以使用yield命令，因此箭头函数不能用作 Generator 函数。
+*/
+// 箭头函数内部没有this
+function foo() {
+  var _this = this;
+
+  setTimeout(function () {
+    console.log('id:', _this.id);
+  }, 100);
+}
+// 例:
+var handler = {
+  id: '123456',
+  init: function init() {
+    var _this2 = this;
+
+    document.addEventListener('click', function (event) {
+      return _this2.doSomething(event.type);
+    }, false);
+  },
+  doSomething: function doSomething(type) {
+    console.log('Handling ' + type + ' for ' + this.id);
+  }
+};
+
+// 除了this，以下三个变量在箭头函数之中也是不存在的，
+// 指向外层函数的对应变量：arguments、super、new.target。
+
+// 嵌套的箭头函数
+// 箭头函数内容在嵌套使用箭头函数
+
+// ES5中的嵌套函数
+function insert(val) {
+  return {
+    into: function into(array) {
+      return {
+        after: function after(afterVal) {
+          array.splic(array.indexOf(afterVal) + 1, 0, val);
+          return array;
+        }
+      };
+    }
+  };
+}
+
+// ES6箭头嵌套函数
+var insert = function insert(val) {
+  return { into: function into(array) {
+      return { after: function after(afterVal) {
+          array.splic(array.indexOf(afterVal) + 1, 0, val);
+          return array;
+        } };
+    } };
+};
+
+insert(2).into([1, 3]).after(1); // 调用： [1,2,3] 
+
+/* 6.双冒号运算符 */
+// +++++++++++++++++++++++++++++++++++++++
--- es6-ryf/src/06.function-expand.js
+++ es6-ryf/src/06.function-expand.js
@@ -83,3 +83,123 @@
 // foo(1)
 
 /* 2.rest 参数 */
+function add(...values){
+	console.log(values);
+	let sum=0;
+	for(let val of values){
+		sum+=val;
+	}
+	return sum;
+}
+// console.log(add(1,8,10));
+
+// rest 参数之后不能再有其他参数（即只能是最后一个参数），否则会报错。
+// 函数的length属性，不包括 rest 参数。
+
+/* 3.严格模式 */
+// ES5
+function doSomething(a,b){
+	"use strice";
+}
+// ES6
+// 规定只要函数参数使用了默认值、解构赋值、或者扩展运算符，
+// 那么函数内部就不能显式设定为严格模式，否则会报错。
+
+/* 4.name 属性 */
+// 函数的name属性，返回该函数的函数名。
+function foo() {}
+foo.name // "foo"
+
+/* 5.箭头函数 */
+// ES6 允许使用“箭头”（=>）定义函数。
+var f=v=>v;
+var f=()=>5;
+var sum=(n1,n2,n3)=>n1+n2+n3;
+var sum=(a,b)=>{return (a+b)*a}
+let fn = () => void doesNotReturn();
+
+// 箭头函数可以与变量解构结合使用
+const full=({first,last})=>first+'&'+last;
+
+// 使表达跟简洁
+const isEven = n => n % 2 == 0;
+const square = n => n * n;
+
+// 用于简化回调函数
+// 正常函数写法
+[1,2,3].map(function (x) {
+  return x * x;
+});
+// 箭头函数写法
+[1,2,3].map(x => x * x);
+
+// 简化排序函数
+// 正常函数写法
+var result = [1,2,4,8].sort(function (a, b) {
+  return a - b;
+});
+// 箭头函数写法
+var result = [1,2,4,8].sort((a, b) => a - b);
+
+// rest 参数与箭头函数结合的例子
+const numbers=(...n)=>n;
+// console.log(numbers(1,2,3,4,5,6));
+
+const headAndTail=(head,...tail)=>[head,tail];
+// console.log(headAndTail(1, 2, 3, 4, 5));
+
+// 箭头函数的使用注意点
+/*
+（1）函数体内的this对象，就是定义时所在的对象，而不是使用时所在的对象。
+（2）不可以当作构造函数，也就是说，不可以使用new命令，否则会抛出一个错误。
+（3）不可以使用arguments对象，该对象在函数体内不存在。如果要用，可以用 rest 参数代替。
+（4）不可以使用yield命令，因此箭头函数不能用作 Generator 函数。
+*/
+// 箭头函数内部没有this
+function foo() {
+  setTimeout(() => {
+    console.log('id:', this.id);
+  }, 100);
+}
+// 例:
+var handler = {
+  id: '123456',
+  init: function() {
+    document.addEventListener('click',
+      event => this.doSomething(event.type), false);
+  },
+  doSomething: function(type) {
+    console.log('Handling ' + type  + ' for ' + this.id);
+  }
+};
+
+// 除了this，以下三个变量在箭头函数之中也是不存在的，
+// 指向外层函数的对应变量：arguments、super、new.target。
+
+// 嵌套的箭头函数
+// 箭头函数内容在嵌套使用箭头函数
+
+// ES5中的嵌套函数
+function insert(val){
+	return {
+		into:function (array){
+			return {
+				after:function(afterVal){
+					array.splic(array.indexOf(afterVal)+1,0,val);
+					return array;
+				}
+			}
+		}
+	}
+}
+
+// ES6箭头嵌套函数
+var insert=(val)=>({into:(array)=>({after:(afterVal)=>{
+	array.splic(array.indexOf(afterVal)+1,0,val);
+	return array;
+}})})
+
+insert(2).into([1,3]).after(1);	// 调用： [1,2,3] 
+
+/* 6.双冒号运算符 */
+// +++++++++++++++++++++++++++++++++++++++
