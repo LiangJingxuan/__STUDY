c20289e7358c75108b1f1b0f529dd66c88077a48
liangjiang <xuan03121994@163.com>
2018-03-20 17:37:47 +0800

    symbol

---
+++ es6-ryf/dist/09.Symbol.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * Symbol
+ * 
+ */
+
+/* 1.概述 */
+/* 2.作为属性名的 Symbol */
+/* 3.实例：消除魔术字符串 */
+/* 4.属性名的遍历 */
+/* 5.Symbol.for()，Symbol.keyFor() */
+/* 6.实例：模块的 Singleton 模式 */
+/* 7.内置的 Symbol 值 */
