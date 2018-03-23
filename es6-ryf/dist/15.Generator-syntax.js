928da5fec8cfa424549c542d33ee52ca429d42f1
liangjingxuan <xuan03121994@163.com>
2018-03-23 23:05:37 +0800

    Generator syntax

---
+++ es6-ryf/dist/15.Generator-syntax.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * Generator 函数的语法
+ * 
+ */
+
+/* 1.简介 */
+/* 2.next 方法的参数 */
+/* 3.for...of 循环 */
+/* 4.Generator.prototype.throw() */
+/* 5.Generator.prototype.return() */
+/* 6.next()、throw()、return() 的共同点 */
+/* 7.yield* 表达式 */
+/* 8.作为对象属性的 Generator 函数 */
+/* 9.Generator 函数的this */
+/* 10.含义 */
+/* 11.应用 */
