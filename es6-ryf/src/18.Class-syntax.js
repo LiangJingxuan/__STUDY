aa3d5f2c425a32fcb4c7779076c1a309f67e6e0f
liangjiang <xuan03121994@163.com>
2018-03-26 15:27:38 +0800

    Class syntax

---
+++ es6-ryf/src/18.Class-syntax.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * Class 的基本语法
+ * 
+ */
+
+/*简介*/
+/*严格模式*/
+/*constructor 方法*/
+/*类的实例对象*/
+/*Class 表达式*/
+/*不存在变量提升*/
+/*私有方法和私有属性*/
+/*this 的指向*/
+/*name 属性*/
+/*Class 的取值函数（getter）和存值函数（setter）*/
+/*Class 的 Generator 方法*/
+/*Class 的静态方法*/
+/*Class 的静态属性和实例属性*/
+/*new.target 属性*/
