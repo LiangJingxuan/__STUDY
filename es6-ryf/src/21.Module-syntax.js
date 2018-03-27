1cd359591da00d7a506571f8e476e2d1c59b6e06
liangjiang <xuan03121994@163.com>
2018-03-27 14:54:27 +0800

    Module syntax

---
+++ es6-ryf/src/21.Module-syntax.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * Module 的语法
+ * 
+ */
+
+/*概述*/
+/*严格模式*/
+/*export 命令*/
+/*import 命令*/
+/*模块的整体加载*/
+/*export default 命令*/
+/*export 与 import 的复合写法*/
+/*模块的继承*/
+/*跨模块常量*/
+/*import()*/
