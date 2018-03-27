6062ce7c6e737d48f9a2dd11729fb4fea5957746
liangjingxuan <xuan03121994@163.com>
2018-03-27 21:30:06 +0800

    Generator syntax

--- es6-ryf/dist/18.Class-syntax.js
+++ es6-ryf/dist/18.Class-syntax.js
@@ -9,6 +9,10 @@
 // 定义类
 // ES6 的类，完全可以看作构造函数的另一种写法
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -16,21 +20,21 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Point = function () {
-	function Point(x, y) {
-		_classCallCheck(this, Point);
+  function Point(x, y) {
+    _classCallCheck(this, Point);
 
-		this.x = x;
-		this.y = y;
-	}
+    this.x = x;
+    this.y = y;
+  }
 
-	_createClass(Point, [{
-		key: 'toString',
-		value: function toString() {
-			return '(' + this.x + ',' + this.y + ')';
-		}
-	}]);
+  _createClass(Point, [{
+    key: 'toString',
+    value: function toString() {
+      return '(' + this.x + ',' + this.y + ')';
+    }
+  }]);
 
-	return Point;
+  return Point;
 }();
 
 ;
@@ -40,18 +44,18 @@ Point === Point.prototype.constructor; // true
 // 使用: 直接对类使用new命令，跟构造函数的用法完全一致
 
 var Bar = function () {
-	function Bar() {
-		_classCallCheck(this, Bar);
-	}
+  function Bar() {
+    _classCallCheck(this, Bar);
+  }
 
-	_createClass(Bar, [{
-		key: 'doStuff',
-		value: function doStuff() {
-			console.log('stuff');
-		}
-	}]);
+  _createClass(Bar, [{
+    key: 'doStuff',
+    value: function doStuff() {
+      console.log('stuff');
+    }
+  }]);
 
-	return Bar;
+  return Bar;
 }();
 
 ;
@@ -61,7 +65,7 @@ b.doStuff(); // stuff
 // 在类的实例上面调用方法，其实就是调用原型上的方法
 
 var B = function B() {
-	_classCallCheck(this, B);
+  _classCallCheck(this, B);
 };
 
 var b2 = new B();
@@ -69,8 +73,8 @@ b2.constructor === B.prototype.constructor; // true
 
 // Object.assign方法可以很方便地一次向类添加多个方法
 Object.assign(Point.prototype, {
-	toString: function toString() {},
-	toValue: function toValue() {}
+  toString: function toString() {},
+  toValue: function toValue() {}
 });
 
 // prototype对象的constructor属性，直接指向“类”的本身，这与 ES5 的行为是一致的
@@ -82,20 +86,20 @@ Point.prototype.constructor === Point; // true
 var methodName = 'getArea';
 
 var Square = function () {
-	function Square(length) {
-		// ...
+  function Square(length) {
+    // ...
 
-		_classCallCheck(this, Square);
-	}
+    _classCallCheck(this, Square);
+  }
 
-	_createClass(Square, [{
-		key: methodName,
-		value: function value() {
-			// ...
-		}
-	}]);
+  _createClass(Square, [{
+    key: methodName,
+    value: function value() {
+      // ...
+    }
+  }]);
 
-	return Square;
+  return Square;
 }();
 
 /*严格模式*/
@@ -116,43 +120,43 @@ var Square = function () {
 
 
 var MyClass = function () {
-	function Me() {
-		_classCallCheck(this, Me);
-	}
+  function Me() {
+    _classCallCheck(this, Me);
+  }
 
-	_createClass(Me, [{
-		key: 'getClassName',
-		value: function getClassName() {
-			return Me.name;
-		}
-	}]);
+  _createClass(Me, [{
+    key: 'getClassName',
+    value: function getClassName() {
+      return Me.name;
+    }
+  }]);
 
-	return Me;
+  return Me;
 }();
 var inst = new MyClass();
 inst.getClassName(); // Me
 
 // 如果类的内部没用到的话，可以省略Me，也就是可以写成下面的形式
 var MyClass2 = function MyClass2() {
-	_classCallCheck(this, MyClass2);
+  _classCallCheck(this, MyClass2);
 };
 
 // 采用 Class 表达式，可以写出立即执行的 Class
 var Person = new (function () {
-	function _class(name) {
-		_classCallCheck(this, _class);
+  function _class(name) {
+    _classCallCheck(this, _class);
 
-		this.name = name;
-	}
+    this.name = name;
+  }
 
-	_createClass(_class, [{
-		key: 'sayName',
-		value: function sayName() {
-			console.log(this.name);
-		}
-	}]);
+  _createClass(_class, [{
+    key: 'sayName',
+    value: function sayName() {
+      console.log(this.name);
+    }
+  }]);
 
-	return _class;
+  return _class;
 }())('张三');
 Person.sayName();
 
@@ -166,30 +170,343 @@ Person.sayName();
 
 /*私有方法和私有属性*/
 
-// 现有的方法  +++++++++++++++++++++++++++++++++++++++++++
+// 现有的方法
 // 一种做法是在命名上加以区别
 
 var Widget = function () {
-	function Widget() {
-		_classCallCheck(this, Widget);
-	}
+  function Widget() {
+    _classCallCheck(this, Widget);
+  }
 
-	_createClass(Widget, [{
-		key: 'foo',
-		value: function foo(baz) {
-			this._bar(baz);
-		}
-	}]);
+  _createClass(Widget, [{
+    key: 'foo',
 
-	return Widget;
+
+    // 公有方法
+    value: function foo(baz) {
+      this._bar(baz);
+    }
+
+    // 私有方法
+
+  }, {
+    key: '_bar',
+    value: function _bar(baz) {
+      return this.snaf = baz;
+    }
+  }]);
+
+  return Widget;
+}();
+
+;
+// 另一种方法就是索性将私有方法移出模块，因为模块内部的所有方法都是对外可见的
+// foo是公有方法，内部调用了bar.call(this, baz)。这使得bar实际上成为了当前模块的私有方法
+
+var Widget2 = function () {
+  function Widget2() {
+    _classCallCheck(this, Widget2);
+  }
+
+  _createClass(Widget2, [{
+    key: 'foo',
+    value: function foo(baz) {
+      bar.call(this, baz);
+    }
+
+    // ...
+
+  }]);
+
+  return Widget2;
+}();
+
+;
+// 还有一种方法是利用Symbol值的唯一性，将私有方法的名字命名为一个Symbol值
+var bar = Symbol('bar');
+var snaf = Symbol('snaf');
+
+var myClass = function () {
+  function myClass() {
+    _classCallCheck(this, myClass);
+  }
+
+  _createClass(myClass, [{
+    key: 'foo',
+
+
+    // 公有方法
+    value: function foo(baz) {
+      this[bar](baz);
+    }
+
+    // 私有方法
+
+  }, {
+    key: bar,
+    value: function value(baz) {
+      return this[snaf] = baz;
+    }
+
+    // ...
+
+  }]);
+
+  return myClass;
+}();
+
+exports.default = myClass;
+;
+
+// 私有属性的提案
+// 与私有方法一样，ES6 不支持私有属性。目前，有一个提案，为class加了私有属性。
+// 方法是在属性名之前，使用#表示
+
+/*this 的指向*/
+// 类的方法内部如果含有this，它默认指向类的实例。但是，必须非常小心，一旦单独使用该方法，很可能报错
+// 一个比较简单的解决方法是，在构造方法中绑定this，这样就不会找不到print方法了
+// 另一种解决方法是使用箭头函数
+// 还有一种解决方法是使用Proxy，获取方法的时候，自动绑定this
+
+/*name 属性*/
+// name属性总是返回紧跟在class关键字后面的类名
+
+var Point3 = function Point3() {
+  _classCallCheck(this, Point3);
+};
+
+;
+Point3.name; // "Point"
+
+/*Class 的取值函数（getter）和存值函数（setter）*/
+// 与 ES5 一样，在“类”的内部可以使用get和set关键字，对某个属性设置存值函数和取值函数，
+// 拦截该属性的存取行为
+
+var MyClass4 = function () {
+  function MyClass4() {
+    // ...
+
+    _classCallCheck(this, MyClass4);
+  }
+
+  _createClass(MyClass4, [{
+    key: 'prop',
+    get: function get() {
+      return 'getter';
+    },
+    set: function set(value) {
+      console.log('setter: ' + value);
+    }
+  }]);
+
+  return MyClass4;
+}();
+
+;
+var inst2 = new MyClass4();
+inst2.prop = 123; // setter: 123
+inst2.prop; // 'getter'
+
+/*Class 的 Generator 方法*/
+
+var Foo = function () {
+  function Foo() {
+    _classCallCheck(this, Foo);
+
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    this.args = args;
+  }
+
+  _createClass(Foo, [{
+    key: Symbol.iterator,
+    value: /*#__PURE__*/regeneratorRuntime.mark(function value() {
+      var _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, arg;
+
+      return regeneratorRuntime.wrap(function value$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              _iteratorNormalCompletion = true;
+              _didIteratorError = false;
+              _iteratorError = undefined;
+              _context.prev = 3;
+              _iterator = this.args[Symbol.iterator]();
+
+            case 5:
+              if (_iteratorNormalCompletion = (_step = _iterator.next()).done) {
+                _context.next = 12;
+                break;
+              }
+
+              arg = _step.value;
+              _context.next = 9;
+              return arg;
+
+            case 9:
+              _iteratorNormalCompletion = true;
+              _context.next = 5;
+              break;
+
+            case 12:
+              _context.next = 18;
+              break;
+
+            case 14:
+              _context.prev = 14;
+              _context.t0 = _context['catch'](3);
+              _didIteratorError = true;
+              _iteratorError = _context.t0;
+
+            case 18:
+              _context.prev = 18;
+              _context.prev = 19;
+
+              if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+              }
+
+            case 21:
+              _context.prev = 21;
+
+              if (!_didIteratorError) {
+                _context.next = 24;
+                break;
+              }
+
+              throw _iteratorError;
+
+            case 24:
+              return _context.finish(21);
+
+            case 25:
+              return _context.finish(18);
+
+            case 26:
+            case 'end':
+              return _context.stop();
+          }
+        }
+      }, value, this, [[3, 14, 18, 26], [19,, 21, 25]]);
+    })
+  }]);
+
+  return Foo;
 }();
 
 ;
 
-/*this 的指向*/
-/*name 属性*/
-/*Class 的取值函数（getter）和存值函数（setter）*/
-/*Class 的 Generator 方法*/
+var _iteratorNormalCompletion2 = true;
+var _didIteratorError2 = false;
+var _iteratorError2 = undefined;
+
+try {
+  for (var _iterator2 = new Foo('hello', 'world')[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+    var x = _step2.value;
+
+    console.log(x);
+  }
+} catch (err) {
+  _didIteratorError2 = true;
+  _iteratorError2 = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion2 && _iterator2.return) {
+      _iterator2.return();
+    }
+  } finally {
+    if (_didIteratorError2) {
+      throw _iteratorError2;
+    }
+  }
+}
+
+;
+// hello
+// world
+
 /*Class 的静态方法*/
+// 如果在一个方法前，加上static关键字，就表示该方法不会被实例继承，而是直接通过类来调用，这就称为“静态方法”
+
+var Foo3 = function () {
+  function Foo3() {
+    _classCallCheck(this, Foo3);
+  }
+
+  _createClass(Foo3, null, [{
+    key: 'classMethod',
+    value: function classMethod() {
+      return 'hello';
+    }
+  }]);
+
+  return Foo3;
+}();
+
+;
+Foo3.classMethod(); // 'hello'
+var foo = new Foo3();
+// foo.classMethod()
+// TypeError: foo.classMethod is not a function
+
+// 如果静态方法包含this关键字，这个this指的是类，而不是实例
+
+var Foo4 = function () {
+  function Foo4() {
+    _classCallCheck(this, Foo4);
+  }
+
+  _createClass(Foo4, [{
+    key: 'baz',
+    value: function baz() {
+      console.log('world');
+    }
+  }], [{
+    key: 'bar',
+    value: function bar() {
+      this.baz();
+    }
+  }, {
+    key: 'baz',
+    value: function baz() {
+      console.log('hello');
+    }
+  }]);
+
+  return Foo4;
+}();
+
+;
+Foo4.bar(); // hello
+
 /*Class 的静态属性和实例属性*/
+// 静态属性指的是 Class 本身的属性，即Class.propName，而不是定义在实例对象（this）上的属性
+
+var Foos = function Foos() {
+  _classCallCheck(this, Foos);
+};
+
+Foos.prop = 1;
+Foos.prop; // 1
+
+// 静态属性的提案
+// 1.类的实例属性可以用等式，写入类的定义之中
+// 2.类的静态属性只要在上面的实例属性写法前面，加上static关键字就可以了
+
 /*new.target 属性*/
+// 该属性一般用在构造函数之中，返回new命令作用于的那个构造函数
+// 如果构造函数不是通过new命令调用的，new.target会返回undefined
+// 这个属性可以用来确定构造函数是怎么调用的
+// 在函数外部，使用new.target会报错
+
+var Rectangle = function Rectangle(length, width) {
+  _classCallCheck(this, Rectangle);
+
+  console.log(new.target === Rectangle);
+  this.length = length;
+  this.width = width;
+};
+
+var obj = new Rectangle(3, 4); // 输出 true
--- es6-ryf/src/18.Class-syntax.js
+++ es6-ryf/src/18.Class-syntax.js
@@ -103,19 +103,145 @@ Person.sayName();
 
 /*私有方法和私有属性*/
 
-// 现有的方法  +++++++++++++++++++++++++++++++++++++++++++
+// 现有的方法
 // 一种做法是在命名上加以区别
 class Widget{
+
+	// 公有方法
 	foo(baz){
 		this._bar(baz);
 	}
+
+	// 私有方法
+	_bar(baz){
+		return this.snaf=baz;
+	}
+};
+// 另一种方法就是索性将私有方法移出模块，因为模块内部的所有方法都是对外可见的
+// foo是公有方法，内部调用了bar.call(this, baz)。这使得bar实际上成为了当前模块的私有方法
+class Widget2 {
+  foo (baz) {
+    bar.call(this, baz);
+  }
+
+  // ...
+};
+// 还有一种方法是利用Symbol值的唯一性，将私有方法的名字命名为一个Symbol值
+const bar = Symbol('bar');
+const snaf = Symbol('snaf');
+export default class myClass{
+
+  // 公有方法
+  foo(baz) {
+    this[bar](baz);
+  }
+
+  // 私有方法
+  [bar](baz) {
+    return this[snaf] = baz;
+  }
+
+  // ...
 };
 
+// 私有属性的提案
+// 与私有方法一样，ES6 不支持私有属性。目前，有一个提案，为class加了私有属性。
+// 方法是在属性名之前，使用#表示
 
 /*this 的指向*/
+// 类的方法内部如果含有this，它默认指向类的实例。但是，必须非常小心，一旦单独使用该方法，很可能报错
+// 一个比较简单的解决方法是，在构造方法中绑定this，这样就不会找不到print方法了
+// 另一种解决方法是使用箭头函数
+// 还有一种解决方法是使用Proxy，获取方法的时候，自动绑定this
+
 /*name 属性*/
+// name属性总是返回紧跟在class关键字后面的类名
+class Point3 {};
+Point3.name; // "Point"
+
 /*Class 的取值函数（getter）和存值函数（setter）*/
+// 与 ES5 一样，在“类”的内部可以使用get和set关键字，对某个属性设置存值函数和取值函数，
+// 拦截该属性的存取行为
+class MyClass4 {
+  constructor() {
+    // ...
+  }
+  get prop() {
+    return 'getter';
+  }
+  set prop(value) {
+    console.log('setter: '+value);
+  }
+};
+let inst2 = new MyClass4();
+inst2.prop = 123; // setter: 123
+inst2.prop; // 'getter'
+
 /*Class 的 Generator 方法*/
+class Foo {
+  constructor(...args) {
+    this.args = args;
+  }
+  * [Symbol.iterator]() {
+    for (let arg of this.args) {
+      yield arg;
+    }
+  }
+};
+
+for (let x of new Foo('hello', 'world')) {
+  console.log(x);
+};
+// hello
+// world
+
 /*Class 的静态方法*/
+// 如果在一个方法前，加上static关键字，就表示该方法不会被实例继承，而是直接通过类来调用，这就称为“静态方法”
+class Foo3 {
+  static classMethod() {
+    return 'hello';
+  }
+};
+Foo3.classMethod() // 'hello'
+var foo = new Foo3();
+// foo.classMethod()
+// TypeError: foo.classMethod is not a function
+
+// 如果静态方法包含this关键字，这个this指的是类，而不是实例
+class Foo4 {
+  static bar () {
+    this.baz();
+  }
+  static baz () {
+    console.log('hello');
+  }
+  baz () {
+    console.log('world');
+  }
+};
+Foo4.bar() // hello
+
 /*Class 的静态属性和实例属性*/
+// 静态属性指的是 Class 本身的属性，即Class.propName，而不是定义在实例对象（this）上的属性
+class Foos {
+}
+Foos.prop = 1;
+Foos.prop // 1
+
+// 静态属性的提案
+// 1.类的实例属性可以用等式，写入类的定义之中
+// 2.类的静态属性只要在上面的实例属性写法前面，加上static关键字就可以了
+
 /*new.target 属性*/
+// 该属性一般用在构造函数之中，返回new命令作用于的那个构造函数
+// 如果构造函数不是通过new命令调用的，new.target会返回undefined
+// 这个属性可以用来确定构造函数是怎么调用的
+// 在函数外部，使用new.target会报错
+class Rectangle {
+  constructor(length, width) {
+    console.log(new.target === Rectangle);
+    this.length = length;
+    this.width = width;
+  }
+}
+var obj = new Rectangle(3, 4); // 输出 true
