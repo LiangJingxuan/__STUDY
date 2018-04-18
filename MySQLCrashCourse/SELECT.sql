aa88512f2a50fe25beb563718397312292d41f2a
liangjiang <xuan03121994@163.com>
2018-04-18 17:53:49 +0800

    mysqlCrashCourse

--- MySQLCrashCourse/SELECT.sql
+++ MySQLCrashCourse/SELECT.sql
@@ -104,8 +104,68 @@ SELECT * FROM food WHERE `class` NOT IN (11,21,32) ORDER BY `id`;
 
 -- LIKE操作符
 
--- 百分号（%）通配符
+-- 百分号（%）通配符(匹配任意字符)
 SELECT * FROM food WHERE `title` LIKE '%水果%';
 
+-- 下划线（_）通配符(匹配一个字符)
+SELECT * FROM food WHERE `title` LIKE `_水果`;
 
--- ---------------------------------+++++++++++++++++++++++++++++++++++++++++++++63页
+-- 使用通配符的技巧
+
+-- 不要过度使用通配符。如果其他操作符能达到相同的目的，应该使用其他操作符。
+
+-- 在确实需要使用通配符时，除非绝对有必要，否则不要把它们用在搜索模式的开始处。把通配符置于搜索模式的开始处，搜索起来是最慢的。
+
+-- 仔细注意通配符的位置。如果放错地方，可能不会返回想要的数据。
+
+
+
+-- 用正则表达式进行搜索
+
+-- 使用MySQL正则表达式
+
+-- 基本字符匹配
+
+-- 检索列prod_name包含文本1000的所有行:
+SELECT * FROM food WHERE `prod_name` REGEXP 1000 ORDER BY `prod_name`; 
+
+-- .是正则表达式语言中一个特殊的字符, 它表示匹配任意一个字符
+SELECT * FROM food WHERE `prod_name` REGEXP '.00' ORDER BY `prod_name`;
+
+-- 为区分大小写，可使用BINARY关键字
+SELECT * FROM food WHERE `prod_name` REGEXP BINARY 'C .';
+
+-- 进行OR匹配
+SELECT * FROM food WHERE `prod_name` REGEXP '1000|2000|3000';
+
+-- 匹配几个字符之一
+SELECT * FROM food WHERE `prod_name` REGEXP '[1000,2000,3000] Ton';
+
+-- 匹配范围
+SELECT * FROM food WHERE `prod_name` REGEXP '[100-500] Ton';
+
+-- 匹配特殊字符
+SELECT * FROM food WHERE `prod_name` REGEXP '\\.';
+
+-- 匹配字符类
+
+-- 匹配多个实例
+SELECT * FROM food WHERE `prod_name` REGEXP '\\([0-9] sticks?\\)';
+
+-- 匹配连在一起的4位数字
+SELECT * FROM food WHERE `prod_name` REGEXP '[[:digit:]]{4}';
+
+-- 定位符
+
+-- 找出以一个数（包括以小数点开始的数）开始的所有产品
+SELECT * FROM food WHERE `prod_name` REGEXP '^[0-9\\.]';
+
+
+
+
+
+-- 创建计算字段
+
+
+
+
